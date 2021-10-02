0cd892b695c5c934746ff4c786c491f5444d7b66
Xenoty <jacqueso.olivier@gmail.com>
2021-10-02 19:01:44 +0200

    Finished off hamburger animation

--- js/main.js
+++ js/main.js
@@ -23,19 +23,45 @@ function ToggleHamburger(className){
 
     hamburgerIsOpen = $(className).css('display') == 'block';
 
-    if(hamburgerLineArray.length == 0){
-        $(".hamburger-icon").children("div").each(function() {
-            hamburgerLineArray.push(this);
-        });
-    }
-
     if(hamburgerIsOpen){
-        hamburgerLineArray[0].className += ' ' + "ham-top-line"; 
-        hamburgerLineArray[1].className += ' ' + "ham-middle-line";
-        hamburgerLineArray[2].className += ' ' + "ham-bottom-line";
+        $(".hamburger-icon").children("div").each(function(index) {
+            AddHamburgerClass(index, this);
+        });
     }else{
-        $(".hamburger-icon").children("div").each(function() {
-            $(this).removeClass("ham-top-line ham-middle-line ham-bottom-line");
+        $(".hamburger-icon").children("div").each(function(index) {
+            RemoveHamburgerClass(index, this);
         });
     }   
+}
+
+function AddHamburgerClass(index, element){
+    switch(index){
+        case 0:
+            $(element).addClass("rotate-top");
+            $(element).removeClass("reset-rotate");
+            break;
+        case 1:
+            $(element).addClass("ham-middle-line");
+            break;
+        case 2:
+            $(element).addClass("rotate-bottom");
+            $(element).removeClass("reset-rotate");
+            break;
+    }
+}
+
+function RemoveHamburgerClass(index, element){
+    switch(index){
+        case 0:
+            $(element).removeClass("rotate-top");
+            $(element).addClass("reset-rotate");
+            break;
+        case 1:
+            $(element).removeClass("ham-middle-line");
+            break;
+        case 2:
+            $(element).removeClass("rotate-bottom");
+            $(element).addClass("reset-rotate");
+            break;
+    }
 }

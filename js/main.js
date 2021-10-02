d6e81296e545482582f9b3f03722d837eef8dc89
Xenoty <jacqueso.olivier@gmail.com>
2021-10-02 17:06:20 +0200

    Hamburger finished

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+const mainTag = document.getElementsByTagName("main");
+var hamburgerLineArray = [];
+var hamburgerIsOpen = false;
 
 $(document).ready(function(){
     InsertComponents();
@@ -11,4 +14,30 @@ function InsertComponents(){
     $.get("../components/footer.html", function(data){
         $("footer").replaceWith(data);
     });
+}
+
+function ToggleHamburger(className){
+
+    $(className).toggle(); 
+    $(mainTag).toggle();
+
+    hamburgerIsOpen = $(className).css('display') == 'block';
+
+    if(hamburgerLineArray.length == 0){
+        $(".hamburger-icon").children("div").each(function() {
+            hamburgerLineArray.push(this);
+        });
+    }
+
+    if(hamburgerIsOpen){
+        hamburgerLineArray[0].className += ' ' + "ham-top-line"; 
+        hamburgerLineArray[1].className += ' ' + "ham-middle-line";
+        hamburgerLineArray[2].className += ' ' + "ham-bottom-line";
+    }else{
+        $(".hamburger-icon").children("div").each(function() {
+            $(this).removeClass("ham-top-line ham-middle-line ham-bottom-line");
+        });
+        // hamburgerLineArray[0].className += ' ' + "reverse-top"; 
+        // hamburgerLineArray[2].className += ' ' + "reverse-bottom"; 
+    }   
 }

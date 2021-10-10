d05e0a5cc60220e9eb20546825839092aeb17861
Xenoty <jacqueso.olivier@gmail.com>
2021-10-10 09:49:15 +0200

    added filter for dates on projects

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,14 @@
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy('./src/style.css');
     eleventyConfig.addPassthroughCopy('./src/main.js');
     eleventyConfig.addPassthroughCopy('./src/assets');
+
+    eleventyConfig.addFilter("projectDate", (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+    })
 
     return {
         dir : {

98118807cf0374128e9f086618f0c6bd862ba12b
Jacques Olivier <jacqueso.olivier@gmail.com>
2021-10-16 09:44:46 +0200

    added passthrough for admin folder

--- .eleventy.js
+++ .eleventy.js
@@ -8,6 +8,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/style.css');
     eleventyConfig.addPassthroughCopy('./src/main.js');
     eleventyConfig.addPassthroughCopy('./src/assets');
+    eleventyConfig.addPassthroughCopy('./src/admin');
 
     //#endregion
 

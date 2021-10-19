950308be96439c940d487d079d5f27f5aa5b94b6
Xenoty <jacqueso.olivier@gmail.com>
2021-10-19 20:44:33 +0200

    custom 404 page added

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ module.exports = function(eleventyConfig) {
 
     //#region PassThrough
 
+    eleventyConfig.addPassthroughCopy('./src/404.html');
     eleventyConfig.addPassthroughCopy('./src/style.css');
     eleventyConfig.addPassthroughCopy('./src/main.js');
     eleventyConfig.addPassthroughCopy('./src/assets');

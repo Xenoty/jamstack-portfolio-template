2ed5ead4b3a253ab954ccc37bf9a48ff2645bb38
Xenoty <jacqueso.olivier@gmail.com>
2021-11-14 15:02:52 +0200

    remove commented out code and neaten up

--- public/markdown.js
+++ public/markdown.js
@@ -1,14 +1,12 @@
-$(document).ready(function() {
-    WrapCodeTagsWithPreTags();
-    AppenBulletListSpanToLists();
-});
+WrapCodeTagsWithPreTags();
+AppenBulletListSpanToLists();
 
 function AppenBulletListSpanToLists(){
   $(".markdown ul, .markdown ol").children().prepend("<span class='bullet-point'></span>")
 }
 
 function WrapCodeTagsWithPreTags(){
-    $("code").parent().each(function(index){
+    $("code").parent().each(function(){
         if(!$(this).is('pre'))
         {
             $(this).children().wrap("<pre class='language-csharp' tabindex='0'></pre>");
